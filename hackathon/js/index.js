e05a9b89f84c40c45b355879be7fbc6b1c6de82e
Siggg <sig@akasig.org>
2018-05-30 12:55:37 +0200

    Update index.js

--- hackathon/js/index.js
+++ hackathon/js/index.js
@@ -1,8 +1,12 @@
-  var secret_seed_1 = lightwallet.keystore.generateRandomSeed();
+  // var secret_seed_1 = lightwallet.keystore.generateRandomSeed();
+
   // var mnemonic = bip39.generateMnemonic()
   // var seed = bip39.mnemonicToSeed(mnemonic)
   // var hdwallet = hdkey.fromMasterSeed(seed)
   var mnemonic = new Mnemonic("english");
+  var secret_words_1 = mnemonic.generate();
+  var passphrase = "";
+  var secret_seed_1 = mnemonic.toSeed(secret_words_1, passphrase);
   
   var account = ethereumjs.WalletHD.fromMasterSeed(secret_seed_1).getWallet();
   var private_key = account.getPrivateKey().toString('hex');
@@ -10,7 +14,7 @@
   var public_address = account.getAddressString();
   var checksum_address = account.getChecksumAddressString();
   // var json_wallet = account.toV3("secret");
-  $('#private_key_seed_1').html(secret_seed_1);
+  $('#private_key_words_1').html(secret_words_1);
   $('#public_address_string_1').html(checksum_address);
   $('#private_key_string_1').html(private_key);
   var public_address_qrcode = new QRCode(document.getElementById("public_address_qr_code_1"), {
