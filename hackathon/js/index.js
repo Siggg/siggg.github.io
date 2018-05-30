c3ff9ed99ee1b247bcb2e824472cd116cfe4a63f
Siggg <sig@akasig.org>
2018-05-30 16:53:55 +0200

    Update index.js

--- hackathon/js/index.js
+++ hackathon/js/index.js
@@ -1,14 +1,16 @@
+var i;
+for (i = 0; i < 10; i++) {
   // var secret_seed_1 = lightwallet.keystore.generateRandomSeed();
 
   // var mnemonic = bip39.generateMnemonic()
   // var seed = bip39.mnemonicToSeed(mnemonic)
   // var hdwallet = hdkey.fromMasterSeed(seed)
   var mnemonic = new Mnemonic("english");
-  var secret_words_1 = mnemonic.generate();
+  var secret_words = mnemonic.generate();
   var passphrase = "verysecret";
-  var secret_seed_1 = mnemonic.toSeed(secret_words_1, passphrase);
-  var hdwallet = ethereumjs.WalletHD.fromMasterSeed(secret_seed_1);
-  // var account = ethereumjs.WalletHD.fromMasterSeed(secret_seed_1).getWallet();
+  var secret_seed = mnemonic.toSeed(secret_words, passphrase);
+  var hdwallet = ethereumjs.WalletHD.fromMasterSeed(secret_seed);
+  // var account = ethereumjs.WalletHD.fromMasterSeed(secret_seed).getWallet();
   var path = "m/44'/60'/0'/0" // Jaxx and Metamask derivation Path
   var node = hdwallet.derivePath(path);
   var account = node.getWallet();
@@ -17,10 +19,20 @@
   // var public_address = account.getAddressString();
   var checksum_address = account.getChecksumAddressString();
   // var json_wallet = account.toV3("secret");
-  // $('#private_key_words_1').html(secret_words_1);
-  $('#public_address_string_1').html(checksum_address);
-  $('#private_key_string_1').html(private_key);
-  var public_address_qrcode = new QRCode(document.getElementById("public_address_qr_code_1"), {
+  // $('#private_key_words').html(secret_words_1);
+  $("#accounts")
+    .find('tbody')
+    .append('<td><span id="public_address_string_' + i
+            + '">En cours de calcul...</span><br /><span id="public_address_qr_code_' + i
+            + '"></span></td>'
+            + '<td><span id="account_index">' + i
+            + '</span></td>'
+            + '<td><span id="private_key_string_' + i
+            + '">En cours de calcul...</span><br /><span id="private_key_qr_code_' + i
+            + '"></span></td>');
+  $('#public_address_string_'+i).html(checksum_address);
+  $('#private_key_string_'+i).html(private_key);
+  var public_address_qrcode = new QRCode(document.getElementById("public_address_qr_code_"+i), {
 	  text: checksum_address,
 	  width: 256,
 	  height: 256,
@@ -28,7 +40,7 @@
 	  colorLight : "#ffffff",
 	  correctLevel : QRCode.CorrectLevel.H
   });
-  var private_key_qrcode = new QRCode(document.getElementById("private_key_qr_code_1"), {
+  var private_key_qrcode = new QRCode(document.getElementById("private_key_qr_code_"+i), {
 	  text: private_key,
 	  width: 256,
 	  height: 256,
@@ -36,3 +48,6 @@
 	  colorLight : "#ffffff",
 	  correctLevel : QRCode.CorrectLevel.H
   });
+} 
+
+
