6ba3b6b1127076af245557e5058cafe65c76f6ab
Siggg <sig@akasig.org>
2018-06-21 01:58:44 +0200

    Update counter.js

--- hackathon/js/counter.js
+++ hackathon/js/counter.js
@@ -26,7 +26,11 @@ This scripts requires the following scripts to be loaded before :
 Here you go. */
 
 // address of the Ethereum account to inspect
-var account_address = "0xD972634E4A036d91d0D4A35EF4927B63ac0Fa7F4";
+var account_address = window.document.URL.searchParams.get("account_address");
+if (account_address == undefined) {
+    var account_address = "0xD972634E4A036d91d0D4A35EF4927B63ac0Fa7F4";
+};
+console.log("account_address :", account_address);
 
 // Please do login to https://etherscan.io/myapikey and get an API key for your pages
 // so that mine does get blocked because of over-use
